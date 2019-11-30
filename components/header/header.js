2800769494e6a79be7e3424f21b857c2e5325caf
Matt <mgould@breakingfreegroup.com>
2019-11-30 12:18:43 +0000

    header selected

--- components/header/header.js
+++ components/header/header.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import './header.css'
 
 const routes = {
-  about: 'about',
-  contact: 'contact',
-  projects: 'projects'
+  about: '/about',
+  contact: '/contact',
+  projects: '/projects'
 }
 
-const HeaderLink = () => {
-
-}
+const HeaderLink = ({ name, selected }) => (
+  <Link href={routes[name]}><p className={selected ? 'selected' : ''}>{name}</p></Link>
+)
 
 const Header = () => {
+  const router = useRouter()
+
   return (
     <header>
       <div className='logo'>
         <Link href='/'><span>LOGO</span></Link>
       </div>
       <div className='header-menu'>
-        {Object.keys(routes).map((r, i) => <Link key={i} href={routes[r]}><p>{r}</p></Link>)}
+        {Object.keys(routes).map((r, i) =>
+          <HeaderLink key={i} name={r} selected={router.pathname === routes[r]} />
+        )}
       </div>
     </header>
   )
--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Error from 'next/error'
+// import Error from 'next/error'
 
 const ErrorPage = () => (
   <div style={{ marginTop: '100px', textAlign: 'center' }}>
