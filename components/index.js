b61bf9c64a63bc530dc8d2f94a4975b4743c32f4
Matt <mgould@breakingfreegroup.com>
2019-12-01 16:59:43 +0000

    component structure

--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
 export { default as Banner } from './banner/banner'
-export { default as Header } from './banner/header'
-export { default as Project } from './banner/project'
+export { default as Header } from './header/header'
+export { default as Project } from './project/project'
