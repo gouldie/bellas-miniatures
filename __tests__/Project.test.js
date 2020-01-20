ffe0863b6a0f94310ec7db93de62b685cc1bb66b
Matt <mgould@breakingfreegroup.com>
2020-01-20 15:25:43 +0000

    nav test

--- __tests__/Project.test.js
+++ __tests__/Project.test.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { Project } from '../components'
+import Router from 'next/router'
 
 afterEach(cleanup)
+
+const routerPushed = jest.fn()
+const mockedRouter = {
+  push: (path, withParam) => {
+    routerPushed(path, withParam)
+    return new Promise((resolve, reject) => resolve())
+  }
+}
+Router.router = mockedRouter
 
 it('renders without crashing', () => {
   const { getByRole } = render(<Project />)
@@ -14,4 +24,10 @@ it('props assigned correctly', () => {
   const { getByRole, getByText } = render(<Project image='testimage' title='testtitle' />)
   expect(getByRole('img')).toHaveAttribute('src', 'testimage')
   expect(getByText('testtitle')).toBeDefined()
+})
+
+it('should navigate on click', async () => {
+  const { getByRole } = render(<Project key={1} id={1} title='testtitle' image='testimage' />)
+  fireEvent.click(getByRole('img'))
+  expect(routerPushed).toHaveBeenCalledWith('/project/[id]', '/project/' + 1)
 })
