10869c956922d068312fac4a1f8734403ffcdcb5
Matt <mgould@breakingfreegroup.com>
2020-02-01 17:58:14 +0000

    flip move

--- components/projects/Project.js
+++ components/projects/Project.js
@@ -1,20 +1,46 @@
 import Link from 'next/link'
+import React, { Component } from 'react'
 
-export default ({ id, title, image }) => (
-  <div className='project-wrapper'>
-    <Link href="/project/[id]" as={`/project/${id}`}>
-      <div className='project'>
-        <img src={image} />
+// export default ({ id, title, image }) => (
+//   <div className='project-wrapper'>
+//     <Link href="/project/[id]" as={`/project/${id}`}>
+//       <div className='project'>
+//         <img src={image} />
+//       </div>
+//     </Link>
+//     <div className='project-info'>
+//       <p>{title}</p>
+//       <div style={{ display: 'flex', alignItems: 'center' }}>
+//         <i className="fas fa-thumbs-up"></i>
+//         <span>26</span>
+//         <i className="fas fa-eye"></i>
+//         <span>982</span>
+//       </div>
+//     </div>
+//   </div>
+// )
+
+export default class Project extends Component {
+  render () {
+    const { title, image, id } = this.props
+
+    return (
+      <div className='project-wrapper'>
+        <Link href="/project/[id]" as={`/project/${id}`}>
+          <div className='project'>
+            <img src={image} />
+          </div>
+        </Link>
+        <div className='project-info'>
+          <p>{title}</p>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <i className="fas fa-thumbs-up"></i>
+            <span>26</span>
+            <i className="fas fa-eye"></i>
+            <span>982</span>
+          </div>
+        </div>
       </div>
-    </Link>
-    <div className='project-info'>
-      <p>{title}</p>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <i className="fas fa-thumbs-up"></i>
-        <span>26</span>
-        <i className="fas fa-eye"></i>
-        <span>982</span>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
--- components/projects/index.js
+++ components/projects/index.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import { Project } from '../../components'
 import Select, { components } from 'react-select'
+import FlipMove from 'react-flip-move'
 
 const options = [
   { value: 'name', label: 'Sort by: Name', subLabel: 'Name' },
@@ -56,12 +57,17 @@ class Projects extends Component {
 
   handleChange = filter => {
     this.setState({ filter })
-    console.log('Option selected:', filter)
   };
 
   render () {
     const { filter } = this.state
     const { projects } = this.props
+
+    if (filter.value === 'name') {
+      projects.sort((a, b) => a.fields.title < b.fields.title ? -1 : 1)
+    } else {
+      projects.sort((a, b) => new Date(a.sys.createdAt) < new Date(b.sys.createdAt) ? 1 : -1)
+    }
 
     return (
       <div className='projects-container'>
@@ -76,16 +82,19 @@ class Projects extends Component {
         </div>
 
         <div className='projects'>
-          {projects.length > 0
-            ? projects.map((p, i) => (
-              <Project
-                key={i}
-                id={p.sys.id}
-                title={p.fields.title}
-                image={p.fields.images[0].fields.file.url + '?fit=pad'}
-              />
-            ))
-            : null}
+          <FlipMove typeName={null}>
+            {projects.length > 0
+              ? projects.map((p, i) => (
+                <Project
+                  id={p.sys.id}
+                  title={p.fields.title}
+                  image={p.fields.images[0].fields.file.url + '?fit=pad'}
+                  key={p.sys.id}
+                />
+              ))
+              : null}
+          </FlipMove>
+
         </div>
       </div>
     )
