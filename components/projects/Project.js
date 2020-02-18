968895ebc9b37a872d3a8f40460067db0eaba0ab
Matt <mgould@breakingfreegroup.com>
2020-02-18 07:55:33 +0000

    remove thumbs up

--- components/projects/Project.js
+++ components/projects/Project.js
@@ -34,8 +34,8 @@ export default class Project extends Component {
         <div className='project-info'>
           <p>{title}</p>
           <div style={{ display: 'flex', alignItems: 'center' }}>
-            <i className="fas fa-thumbs-up"></i>
-            <span>26</span>
+            {/* <i className="fas fa-thumbs-up"></i>
+            <span>26</span> */}
             <i className="fas fa-eye"></i>
             <span>982</span>
           </div>
