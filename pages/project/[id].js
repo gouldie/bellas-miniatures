c7eaf7177c38b49f97513cef6205490c603361e3
Matt <mgould@breakingfreegroup.com>
2020-02-21 16:42:56 +0000

    hooks

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -1,60 +1,49 @@
-import { Component } from 'react'
-import { withRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import ImageGallery from 'react-image-gallery'
+import { Loader } from '../../components'
 import '../../public/sass/project.scss'
 import 'react-image-gallery/styles/scss/image-gallery.scss'
-import { Loader } from '../../components'
 
 const client = require('contentful').createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 })
 
-class Project extends Component {
-  constructor () {
-    super()
+export default () => {
+  const [project, setProject] = useState(null)
+  const router = useRouter()
 
-    this.state = {
-      project: null
-    }
-  }
-
-  async componentDidMount () {
-    const id = this.props.router.query.id
+  useEffect(() => {
+    const id = router.query.id
 
     client.getEntry(id)
       .then(res => {
-        this.setState({ project: res.fields })
+        setProject(res.fields)
       })
       .catch(err => {
         console.log('err', err)
-        this.setState({ project: false })
+        setProject(false)
       })
-  }
+  }, [])
 
-  render () {
-    const { project } = this.state
+  const images = project && project.images.map(i => ({
+    original: i.fields.file.url,
+    thumbnail: i.fields.file.url,
+    sizes: '100px'
+  }))
 
-    const images = project && project.images.map(i => ({
-      original: i.fields.file.url,
-      thumbnail: i.fields.file.url,
-      sizes: '100px'
-    }))
-
-    return (
-      <div className='project-container'>
-        {project === null && <Loader margin='30px 0 0' />}
-        {project === false && <p>No project found.</p>}
-        {project &&
-          <div>
-            <h1>{project.title}</h1>
-            <ImageGallery items={images} />
-            <p>{project.description}</p>
-          </div>
-        }
-      </div>
-    )
-  }
+  return (
+    <div className='project-container'>
+      {project === null && <Loader margin='30px 0 0' />}
+      {project === false && <p>No project found.</p>}
+      {project &&
+        <div>
+          <h1>{project.title}</h1>
+          <ImageGallery items={images} />
+          <p>{project.description}</p>
+        </div>
+      }
+    </div>
+  )
 }
-
-export default withRouter(Project)
