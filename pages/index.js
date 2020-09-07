ee64ca8815c844c97b5f5d8feb7d33d2a54bb16a
Matt <matt@Matts-MacBook-Pro.local>
2020-09-07 06:32:37 +0100

    pre render home pagetext

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import '../public/sass/home.scss'
 import useSWR from 'swr'
 import { Loader } from '../components'
 
-export default () => {
+export default ({ text }) => {
   const { data: galleryImages } = useSWR('galleryImage', fetchEntries)
-  const { data: text } = useSWR('text', fetchEntries)
+  // const { data: text } = useSWR('text', fetchEntries)
 
   const homePageText = text && text.find(t => t.fields.name === 'Home Page Title').fields.text
 
@@ -16,4 +16,21 @@ export default () => {
       {galleryImages && <Home projects={galleryImages} text={homePageText} />}
     </div>
   )
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const text = await fetchEntries('text')
+  // const posts = await res.json()
+
+  // console.log('res', res)
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      text,
+    },
+  }
 }
